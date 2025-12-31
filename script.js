81a820b0f6e92dbce69fbd75aa45b825074aefa8
setusingh6 <singhsetu2005@gmail.com>
2025-12-31 14:49:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+ const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+ 
+ 
+ 
+ function videoconanimation(){
+ var videocon=document.querySelector("#video-container");
+ var playbtn=document.querySelector("#play")
+ videocon.addEventListener("mouseenter",function(){
+      gsap.to(playbtn,{
+          scale:1,
+          opacity:1,
+      })
+
+ })
+ videocon.addEventListener("mouseleave",function(){
+    gsap.to(playbtn,{
+        scale:0,
+        opacity:0,
+    })
+ })
+videocon.addEventListener("mousemove",function(dets){
+    gsap.to(playbtn,{
+        left:dets.x-50,
+        top:dets.y-80,
+    })
+})
+ }
+ videoconanimation()
+
+
+ function loadinganimation(){
+ gsap.from("#page1 h1",{
+       y:100,
+       opacity:0,
+       delay:0.3,
+       duration:0.5,
+       stagger:0.2,
+    })
+ gsap.from("#page1 #video-container",{
+       scale:0.9,
+       opacity:0,
+       delay:1.0,
+       duration:0.3,
+    })
+}
+loadinganimation()
+  document.addEventListener("mousemove",function(dets){
+      gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y,
+      })
+  })
+ document.querySelectorAll(".child").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+  });
+
